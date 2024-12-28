ca743bccf8250375b4437c3853fd948c2c4d0e7d
lilyyllyyllyly <lilylylith@gmail.com>
2024-12-28 13:54:41 -0300

    Update raylib and add rectangle color

--- include/render/mason_rectangle.h
+++ include/render/mason_rectangle.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <raylib.h>
+
 #include "scaffold.h"
 #include "render/mason_sprite.h"
 
@@ -8,7 +10,9 @@ extern int mason_rectangle_type;
 typedef struct {
 	mason_sprite_data* sprite;
 	scaffold_vector2 size;
+	Color color;
 } mason_rectangle_data;
 
 scaffold_node* mason_rectangle_create(scaffold_node* drawer, int draw_order, scaffold_vector2 size);
+void mason_rectangle_set_color(scaffold_node* rect, Color color);
 
--- src/render/rectangle.c
+++ src/render/rectangle.c
@@ -9,6 +9,10 @@ int mason_rectangle_type = NODE_TYPE_UNASSIGNED;
 
 #include "render/mason_rectangle.h"
 
+void mason_rectangle_set_color(scaffold_node* rect, Color color) {
+	((mason_rectangle_data*)(rect->data))->color = color;
+}
+
 static void destroy(scaffold_node* rectangle) {
 	mason_rectangle_data* data = (mason_rectangle_data*)(rectangle->data);
 
@@ -19,8 +23,9 @@ static void destroy(scaffold_node* rectangle) {
 }
 
 static void draw(scaffold_node* rect) {
-	scaffold_vector2 size = ((mason_rectangle_data*)rect->data)->size;
-	DrawRectangle(rect->global_pos.x, rect->global_pos.y, size.x, size.y, BLACK);
+	mason_rectangle_data* rect_d = rect->data;
+	scaffold_vector2 size = rect_d->size;
+	DrawRectangle(rect->global_pos.x, rect->global_pos.y, size.x, size.y, rect_d->color);
 }
 
 scaffold_node* mason_rectangle_create(scaffold_node* drawer, int draw_order, scaffold_vector2 size) {
@@ -34,6 +39,7 @@ scaffold_node* mason_rectangle_create(scaffold_node* drawer, int draw_order, sca
 	);
 
 	data->size = size;
+	data->color = BLACK;
 	data->sprite = mason_sprite_data_create(rectangle, drawer, draw_order, draw);
 
 	return rectangle;
